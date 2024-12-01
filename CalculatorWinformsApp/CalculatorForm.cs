e918ca20cf3177f3a5823feec9d85df81fd9ca9f
WinstonTran1 <winston.tran@mail.mcgill.ca>
2024-11-30 21:28:25 -0500

    Added exception handling for user syntaxic errors, and allowed chaining of result calculations when inputing another operator an operand to a sequence.

--- CalculatorWinformsApp/CalculatorForm.cs
+++ CalculatorWinformsApp/CalculatorForm.cs
@@ -4,30 +4,44 @@ namespace CalculatorWinformsApp
 {
     public partial class formCalculator : Form
     {
-        private string? OperatorToExecute { get; set; }
+        private string? OperatorToExecute { get; set; } = null;
         private bool SwitchToSecondOperand { get; set; } = false;
-        private float? FirstOperand { get; set; }
-        private float? SecondOperand { get; set; }
+        private float? FirstOperand { get; set; } = null;
+        private float? SecondOperand { get; set; } = null;
+        private float? result { get; set; } = null;
 
         public formCalculator()
         {
             InitializeComponent();
         }
 
-        private void Pad(string PadType, string input)
+        private void Pad(string PadType, string input = "")
         {
-            if (calculatorDisplay.Text == "Error")
-                return;
-            switch (PadType)
+            // for this simple project, use a general try catch to catch syntaxical 
+            // user errors
+            try
             {
-                case "num":
-                    numPad(input);
-                    break;
+                if (calculatorDisplay.Text == "Error")
+                    return;
+                switch (PadType)
+                {
+                    case "num":
+                        numPad(input);
+                        break;
 
-                case "op":
-                    operatorPad(input);
-                    break;
-            }   
+                    case "op":
+                        operatorPad(input);
+                        break;
+
+                    case "eq":
+                        eqPad();
+                        break;
+                }
+            }
+            catch 
+            {
+                calculatorDisplay.Text = "Error";
+            }
         }
 
         private void numPad(string number)
@@ -43,10 +57,53 @@ namespace CalculatorWinformsApp
 
         private void operatorPad(string operatorValue)
         // function that handles operator buttons
-        {   
+        {
+            // An operator and both operands are already defined
+            if (this.FirstOperand != null && this.SwitchToSecondOperand == false)
+            {
+                Pad("eq");
+                this.OperatorToExecute = operatorValue;
+                this.SwitchToSecondOperand = true;
+                return;
+            }
             this.OperatorToExecute = operatorValue;
             this.SwitchToSecondOperand = true;
             this.FirstOperand = float.Parse(calculatorDisplay.Text, CultureInfo.InvariantCulture.NumberFormat);
+        }
+
+        private void eqPad()
+        {
+            if (this.SwitchToSecondOperand == true || OperatorToExecute == null)
+            {
+                return;
+            }
+            this.SecondOperand = float.Parse(calculatorDisplay.Text, CultureInfo.InvariantCulture.NumberFormat);
+            switch (this.OperatorToExecute)
+            {
+                case "-":
+                    this.result = this.FirstOperand - this.SecondOperand;
+                    break;
+
+                case "+":
+                    this.result = this.FirstOperand + this.SecondOperand;
+                    break;
+
+                case "*":
+                    this.result = this.FirstOperand * this.SecondOperand;
+                    break;
+
+                case "/":
+                    if (this.SecondOperand == 0)
+                    {
+                        calculatorDisplay.Text = "Error";
+                        return;
+                    }
+                    else
+                        this.result = this.FirstOperand / this.SecondOperand;
+                    break;
+            }
+            calculatorDisplay.Text = this.result.ToString();
+            this.FirstOperand = this.result;
         }
 
         // Numpad inputs
@@ -110,12 +167,6 @@ namespace CalculatorWinformsApp
             Pad("num", "-");
         }
 
-        private void buttonClear_Click(object sender, EventArgs e)
-        {
-            this.OperatorToExecute = null;
-            calculatorDisplay.Text = "";
-        }
-
         
         // Operations
         private void addButton_Click(object sender, EventArgs e)
@@ -140,35 +191,17 @@ namespace CalculatorWinformsApp
 
         private void equalButton_Click(object sender, EventArgs e)
         {
-            if (calculatorDisplay.Text == "Error")
-                return;
-            this.SecondOperand = float.Parse(calculatorDisplay.Text, CultureInfo.InvariantCulture.NumberFormat);
-            float? result = null;
-            switch (this.OperatorToExecute)
-            {
-                case "-":
-                    result = this.FirstOperand - this.SecondOperand;
-                    break;
+            Pad("eq");
+        }
 
-                case "+":
-                    result = this.FirstOperand + this.SecondOperand;
-                    break;
-
-                case "*":
-                    result = this.FirstOperand * this.SecondOperand;
-                    break;
-
-                case "/":
-                    if (this.SecondOperand == 0)
-                    {
-                        calculatorDisplay.Text = "Error";
-                        return;
-                    }
-                    else
-                        result = this.FirstOperand / this.SecondOperand;
-                    break;
-            }
-            calculatorDisplay.Text = result.ToString();
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            this.OperatorToExecute = null;
+            this.SwitchToSecondOperand = false;
+            this.FirstOperand = null;
+            this.SecondOperand = null;
+            this.result = null;
+            calculatorDisplay.Text = "";
         }
     }
 }
